afe6f9bf4c2a9ded0a6e5666aefec83e22ab4c7e
okhamena azeez <86110727+azeezkenny007@users.noreply.github.com>
2022-12-24 03:16:52 +0100

    made adjustment to the code

--- film/account/views.py
+++ film/account/views.py
@@ -43,3 +43,11 @@ def view2(request):
 def man(request):
     logout(request) 
     return redirect(view1)
+
+def test_cookie(request):   
+    if not request.COOKIES.get('color'):
+        response = HttpResponse("Cookie Set")
+        response.set_cookie('color', 'blue')
+        return response
+    else:
+        return HttpResponse("Your favorite color is {0}".format(request.COOKIES['color']))
