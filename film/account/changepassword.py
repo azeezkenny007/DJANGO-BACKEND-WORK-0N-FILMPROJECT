4c95eee43da9d1c17d29302675b020ad0eb1d79c
okhamena azeez <86110727+azeezkenny007@users.noreply.github.com>
2022-12-24 03:08:42 +0100

    added another way to change password

--- film/account/changepassword.py
+++ film/account/changepassword.py
@@ -28,3 +28,21 @@ def password_change_view(request):
     else:
         form = CustomPasswordChangeForm(request.user)
     return render(request, 'password_change.html', {'form': form})
+   
+   
+   # 2nd example 
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import PasswordChangeForm
+
+def change_password(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)  # Important!
+            return redirect('success')
+    else:
+        form = PasswordChangeForm(request.user)
+    return render(request, 'change_password.html', {'form': form})
+
