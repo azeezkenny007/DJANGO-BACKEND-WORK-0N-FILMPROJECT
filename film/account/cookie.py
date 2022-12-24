477e650c7d5e6042d3ef799caf04e8a465124858
okhamena azeez <86110727+azeezkenny007@users.noreply.github.com>
2022-12-24 03:16:04 +0100

    added the cookie functionality

---
+++ film/account/cookie.py
@@ -0,0 +1,9 @@
+from django.http import HttpResponse
+
+def test_cookie(request):   
+    if not request.COOKIES.get('color'):
+        response = HttpResponse("Cookie Set")
+        response.set_cookie('color', 'blue')
+        return response
+    else:
+        return HttpResponse("Your favorite color is {0}".format(request.COOKIES['color']))
