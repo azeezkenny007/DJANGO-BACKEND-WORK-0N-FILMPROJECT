9078e474f8e8adc48755818d7bd5573b979105cd
okhamena azeez <86110727+azeezkenny007@users.noreply.github.com>
2022-12-24 15:53:53 +0100

    added the way the view would look like

---
+++ film/pages/contactus.py
@@ -0,0 +1,19 @@
+from django.contrib import *
+# views.py
+from django.shortcuts import render, redirect
+from .forms import ContactForm
+
+def contact_view(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.add_message(request,messages.DEBUG,"your login is successful")
+            return redirect('success')
+          
+    else:
+        form = ContactForm()
+    return render(request, 'contact.html', {'form': form})
+
+def success_view(request):
+    return render(request, 'success.html')
