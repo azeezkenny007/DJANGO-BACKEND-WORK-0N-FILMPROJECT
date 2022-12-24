0dc30e5c5e636f5f3d54671af34d1f6c58d8dab9
okhamena azeez <86110727+azeezkenny007@users.noreply.github.com>
2022-12-24 03:23:23 +0100

    added another way to get cookie

--- film/account/cookie.py
+++ film/account/cookie.py
@@ -7,3 +7,15 @@ def test_cookie(request):
         return response
     else:
         return HttpResponse("Your favorite color is {0}".format(request.COOKIES['color']))
+       
+# example 2 
+from django.http import HttpResponse
+
+def set_cookie_view(request):
+    response = HttpResponse('Cookie set')
+    response.set_cookie('favorite_color', 'blue', max_age=30)
+    return response
+
+def get_cookie_view(request):
+    favorite_color = request.COOKIES.get('favorite_color')
+    return HttpResponse(f'Your favorite color is {favorite_color}')
