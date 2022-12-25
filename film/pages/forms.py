c495f39c8d9464f44470c76058e656c57c464107
okhamena azeez <86110727+azeezkenny007@users.noreply.github.com>
2022-12-25 10:04:52 +0100

    added the attendance html

--- film/pages/forms.py
+++ film/pages/forms.py
@@ -1,23 +1,19 @@
 # forms.py
 from django.forms import ModelForm
-from .models import Contact,Attendance
+from .models import Contact, Attendance
 from django.contrib import messages
 
 
 from django.db import models
 
 
-
 class ContactForm(ModelForm):
     class Meta:
         model = Contact
         fields = ['name', 'email', 'message']
-        
-
 
 
 class AttendanceForm(ModelForm):
     class Meta:
         model = Attendance
         fields = ['course', 'student']
-
