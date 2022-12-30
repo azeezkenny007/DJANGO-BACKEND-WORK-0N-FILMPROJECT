818858f84e7ddc42924887b731b1615e207a705d
okhamena azeez <86110727+azeezkenny007@users.noreply.github.com>
2022-12-30 19:07:49 +0100

    rectified the error

--- dom/dom/urls.py
+++ dom/dom/urls.py
@@ -19,6 +19,6 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
        path('account/',include('account.urls')),
-     path('pages/',include('pages.urls'))
+     path('pages/',include('pages.urls')),
      path('snippets/',include('snippets.urls'))
 ]
