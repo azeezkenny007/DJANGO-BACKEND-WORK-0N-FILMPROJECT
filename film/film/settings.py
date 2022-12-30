882ad2f546cac0cc2245059884c353effaca7779
okhamena azeez <86110727+azeezkenny007@users.noreply.github.com>
2022-12-30 06:51:20 +0100

    added the rest_framework library

--- film/film/settings.py
+++ film/film/settings.py
@@ -51,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+     'rest_framework',
 ]
 
 ROOT_URLCONF = 'film.urls'
