8f14e79fdb5c4fdfdf9667946109ab4224d9321b
okhamena azeez <86110727+azeezkenny007@users.noreply.github.com>
2022-12-31 10:43:55 +0100

    added a suffix_prefixes

--- dom/snippets/urls.py
+++ dom/snippets/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from .views import *
+from rest_framework.urlpatterns import apply_suffix_patterns
 
 urlpatterns = [
     path('',snippet_list,name="snippets"),
