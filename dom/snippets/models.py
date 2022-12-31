06acb1e72e9fa5934fb8f6b9b5f1990c2bc1ef2a
okhamena azeez <86110727+azeezkenny007@users.noreply.github.com>
2022-12-31 19:41:48 +0100

    added a student model

--- dom/snippets/models.py
+++ dom/snippets/models.py
@@ -30,4 +30,20 @@ class Snippet(models.Model):
         
     def __str__(self):
       return self.title
-  
+
+
+
+class Student(models.Model):
+    # Fields to store student information, such as name, enrollment number, etc.
+    name = models.CharField(max_length=100)
+    enrollment_number = models.CharField(max_length=20, unique=True)
+    # Other fields and methods go here
+
+class Attendance(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    date = models.DateField()
+    present = models.BooleanField(default=True)
+    
+    class Meta:
+        unique_together = ('student', 'date')
+
