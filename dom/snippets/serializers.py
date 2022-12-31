70f07bb8516f585a40055cc1a6e34d492b4ee965
okhamena azeez <86110727+azeezkenny007@users.noreply.github.com>
2022-12-31 12:43:50 +0100

    added the fields into the serializers

--- dom/snippets/serializers.py
+++ dom/snippets/serializers.py
@@ -2,7 +2,7 @@ from .models import Snippet,NameCategory
 from rest_framework.serializers import ModelSerializer,PrimaryKeyRelatedField,StringRelatedField
 
 
-class SnippetSerializer(ModelSerializer):
+class SnippetSerializer(ModelSerializer): 
     class Meta:
         model = Snippet
         fields = "__all__"
