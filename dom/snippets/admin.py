906f0c8715f4aaf22cba190925c5cd76402f8f1d
okhamena azeez <86110727+azeezkenny007@users.noreply.github.com>
2022-12-31 12:05:40 +0100

    registered the site in the admin board

--- dom/snippets/admin.py
+++ dom/snippets/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Snippet
+from .models import Snippet,NameCategory
 
 # Register your models here.
 admin.site.register(Snippet)
+admin.site.register(NameCategory)
