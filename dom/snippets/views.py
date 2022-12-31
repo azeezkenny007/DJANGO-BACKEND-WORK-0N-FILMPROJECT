7d4787f1856be83dc1d959df30f3c68eb2d8e3a3
okhamena azeez <86110727+azeezkenny007@users.noreply.github.com>
2022-12-31 10:13:19 +0100

    imported status from rest_framework

--- dom/snippets/views.py
+++ dom/snippets/views.py
@@ -4,6 +4,7 @@ from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
+from rest_framework import status
 
 # Create your views here.
 
