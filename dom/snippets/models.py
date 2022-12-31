101fac25c36f71b5a7a2615220dc1b94f8b22bc8
okhamena azeez <86110727+azeezkenny007@users.noreply.github.com>
2022-12-31 12:04:45 +0100

    migration added successfully

--- dom/snippets/models.py
+++ dom/snippets/models.py
@@ -8,9 +8,14 @@ LEXERS = [item for item in get_all_lexers() if item[1]]
 LANGUAGE_CHOICES = sorted([(item[1][0], item[0]) for item in LEXERS])
 STYLE_CHOICES = sorted([(item, item) for item in get_all_styles()])
 
+class NameCategory(models.Model):
+    category = models.CharField(max_length=200)
+    name = models.CharField(max_length=300)
+
 
 class Snippet(models.Model):
     created = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey(NameCategory,blank=True, null=True,on_delete=models.CASCADE)
     title = models.CharField(max_length=100, blank=True, default='')
     code = models.TextField()
     linenos = models.BooleanField(default=False)
@@ -22,3 +27,4 @@ class Snippet(models.Model):
         
     def __str__(self):
       return self.title
+  
