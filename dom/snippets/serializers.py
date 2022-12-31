9d561b6411eef488f2557358d1b48abbd88098e8
okhamena azeez <86110727+azeezkenny007@users.noreply.github.com>
2022-12-31 12:25:22 +0100

    modified the serilizer to get all data

--- dom/snippets/serializers.py
+++ dom/snippets/serializers.py
@@ -1,8 +1,8 @@
-from .models import Snippet
-from rest_framework.serializers import ModelSerializer
+from .models import Snippet,NameCategory
+from rest_framework.serializers import ModelSerializer,PrimaryKeyRelatedField,StringRelatedField
 
 
 class SnippetSerializer(ModelSerializer):
     class Meta:
         model = Snippet
-        fields = ['id', 'title', 'code', 'linenos', 'language', 'style',"category"]
+        fields = "__all__"
