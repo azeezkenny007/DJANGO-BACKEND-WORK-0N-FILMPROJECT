20109ca25ec14bfdd19277207401bf3c59da245c
okhamena azeez <86110727+azeezkenny007@users.noreply.github.com>
2022-12-31 19:44:43 +0100

    made migrations

--- dom/snippets/admin.py
+++ dom/snippets/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Snippet,NameCategory
+from .models import Snippet,NameCategory,Attendance,Student
 
 # Register your models here.
 admin.site.register(Snippet)
 admin.site.register(NameCategory)
+admin.site.register(Attendance)
+admin.site.register(Student)
