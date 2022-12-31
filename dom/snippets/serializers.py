48f5e69d565e40e61f245be954dff92fe7aebaa7
okhamena azeez <86110727+azeezkenny007@users.noreply.github.com>
2022-12-31 12:10:41 +0100

    added a new field to the api created

--- dom/snippets/serializers.py
+++ dom/snippets/serializers.py
@@ -5,4 +5,4 @@ from rest_framework.serializers import ModelSerializer
 class SnippetSerializer(ModelSerializer):
     class Meta:
         model = Snippet
-        fields = ['id', 'title', 'code', 'linenos', 'language', 'style']
+        fields = ['id', 'title', 'code', 'linenos', 'language', 'style',"category"]
