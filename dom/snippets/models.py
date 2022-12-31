33bd207d82d33c507d5c734683f56df945092363
okhamena azeez <86110727+azeezkenny007@users.noreply.github.com>
2022-12-31 19:58:56 +0100

    added what the view should appear like in the database

--- dom/snippets/models.py
+++ dom/snippets/models.py
@@ -38,6 +38,9 @@ class Student(models.Model):
     name = models.CharField(max_length=100)
     enrollment_number = models.CharField(max_length=20, unique=True)
     # Other fields and methods go here
+    
+    def __str__(self):
+        return f"{self.name}"
 
 class Attendance(models.Model):
     student = models.ForeignKey(Student, on_delete=models.CASCADE)
@@ -46,4 +49,7 @@ class Attendance(models.Model):
     
     class Meta:
         unique_together = ('student', 'date')
+        
+    def __str__(self):
+        return f"{self.student}"
 
