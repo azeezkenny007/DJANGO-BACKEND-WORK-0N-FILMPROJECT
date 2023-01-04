fecf2b39f3609cfaad4b7bb3a1c25afb38aa6f30
okhamena azeez <86110727+azeezkenny007@users.noreply.github.com>
2023-01-04 20:34:50 +0100

    registered the Todo model model in the admin board

--- dom/snippets/admin.py
+++ dom/snippets/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Snippet,NameCategory,Attendance,Student
+from .models import Snippet,NameCategory,Attendance,Student,Todo
 
 # Register your models here.
 admin.site.register(Snippet)
 admin.site.register(NameCategory)
 admin.site.register(Attendance)
 admin.site.register(Student)
+admin.site.register(Todo)
