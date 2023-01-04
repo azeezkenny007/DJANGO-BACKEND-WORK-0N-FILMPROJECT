4a4ba2130adee14d58414ee8a83413b94a0643ac
okhamena azeez <86110727+azeezkenny007@users.noreply.github.com>
2023-01-04 19:14:08 +0100

    feat:Api view

--- dom/snippets/views.py
+++ dom/snippets/views.py
@@ -81,8 +81,6 @@ class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
-
-
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
