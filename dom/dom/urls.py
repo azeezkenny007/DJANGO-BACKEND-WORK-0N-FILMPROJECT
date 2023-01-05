40b88eb1f428b94332a20c17e604716327b2ef45
okhamena azeez <86110727+azeezkenny007@users.noreply.github.com>
2023-01-05 16:15:20 +0100

    added a url path

--- dom/dom/urls.py
+++ dom/dom/urls.py
@@ -17,8 +17,8 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-       path('account/',include('account.urls')),
+     path('admin/', admin.site.urls),
+     path('account/',include('account.urls')),
      path('pages/',include('pages.urls')),
      path('snippets/',include('snippets.urls'))
 ]
