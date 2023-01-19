aea026eda2f731e43e75d04a85a1ae1897bb9072
okhamena azeez <86110727+azeezkenny007@users.noreply.github.com>
2023-01-19 02:17:53 +0100

    modified the code

--- dom/pages/tests.py
+++ dom/pages/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from django.urls import reverse
 
 # Create your tests here.
+
+
+class MyTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        print("successfully set the data")
+        pass
+
+    def setUp(self):
+        print("set to run everytime")
+        pass
+
+    def test_check_If_the_template_is_okay(self):
+        self.assertTrue(True)
+
+    def test_if_endpoint_correct(self):
+        response = self.client.get('/pages/index2/')
+        self.assertEqual(response.status_code, 200)
